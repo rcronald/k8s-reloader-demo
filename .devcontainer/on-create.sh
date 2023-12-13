f3215ae73340892d30a3e7a868a8e7caa29b33c4
Ronald Requena <rcronald@gmail.com>
2023-12-13 05:48:47 +0000

    updated

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "on-create start"
+echo "$(date +'%Y-%m-%d %H:%M:%S')    on-create start" >> "$HOME/status"
+
+
+
+# clone repos
+
+export REPO_BASE=$PWD
+export PATH="$PATH:$REPO_BASE/cli"
+
+mkdir -p "$HOME/.ssh"
+
+{
+    # add cli to path
+    echo "export PATH=\$PATH:$REPO_BASE/cli"
+
+    echo "export REPO_BASE=$REPO_BASE"
+    echo "compinit"
+} >> "$HOME/.zshrc"
+
+# Generating completitions
+echo "generating completions"
+kic completion zsh > "$HOME/.oh-my-zsh/completions/_kic"
+kubectl completion zsh > "$HOME/.oh-my-zsh/completions/_kubectl"
+
+# Create local registry
+echo "create a local registry"
+docker network create k3d
+k3d registry create registry.localhost --port 5500
+docker network connect k3d k3d-registry.localhost
+
+echo "creating k3d cluster"
+kic cluster rebuild
+
+echo "deploying k3d cluster"
+kic cluster deploy
+
+
+# Only run apt upgrade on pre-build
+if [ "$CODESPACE_NAME" = "null" ]
+then
+    sudo apt-get update
+    sudo apt-get upgrade -y
+    sudo apt-get autoremove -y
+    sudo apt-get clean -y
+fi
+
+echo "on-create complete"
+echo "$(date +'%Y-%m-%d %H:%M:%S')    on-create complete" >> "$HOME/status"
