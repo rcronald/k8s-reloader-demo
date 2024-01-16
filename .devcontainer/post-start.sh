b16aa81d72a9e208c73e390d8f892785cdc032a0
Ronald Requena <rcronald@gmail.com>
2024-01-16 08:06:27 +0000

    updates

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -6,6 +6,23 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
 
 # update the base docker images
 
+# install argocd CLI
+sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/v2.1.5/argocd-linux-amd64
+sudo chmod +x /usr/local/bin/argocd
+argocd help
+#kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+#argocd login humble-computing-machine-xrjr4ppjqr2vp6g-30004.app.github.dev --insecure
+#argocd version
+#argocd app list
+
+#argocd app create demo2 \
+#--project default \
+#--repo https://github.com/codefresh-contrib/gitops-certification-examples \
+#--path "./simple-app" \
+#--dest-namespace default \
+#--dest-server https://kubernetes.default.svc
+
+#argocd app sync demo2
 
 echo "post-start complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start complete" >> "$HOME/status"
